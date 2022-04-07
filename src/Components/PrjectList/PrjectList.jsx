a9d2af792a4febc347aabe70c786eb1d1337baca
Sharoz Ahmed <sharoz.inocent@gmail.com>
2022-04-08 02:01:35 +0500

    second-commit

--- src/Components/PrjectList/PrjectList.jsx
+++ src/Components/PrjectList/PrjectList.jsx
@@ -4,6 +4,7 @@ import './PrjectList.css'
 export default function PrjectList({ list }) {
     return (
         <div>
+            {/* Project List That were created */}
             {list.length === 0 && <p>No Projects yet!</p>}
             {
                 list.map((doc, key) => (
