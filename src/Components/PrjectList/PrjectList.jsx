276f89f3064273dd3ad0e90c8e74a0e669a06289
faraax <suny.faraz@gmail.com>
2022-04-08 16:01:23 +0500

    second commit

--- src/Components/PrjectList/PrjectList.jsx
+++ src/Components/PrjectList/PrjectList.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './PrjectList.css'
+import Avatar from '../Avatar/Avatar'
 
 export default function PrjectList({ list }) {
     return (
-        <div>
+        <div className="project-list">
             {/* Project List That were created */}
             {list.length === 0 && <p>No Projects yet!</p>}
             {
                 list.map((doc, key) => (
-                    <div key={key}>
-                        {doc.name}
-                    </div>
+                    <Link key={key} to={`/project/${doc.id}`}>
+                        <h4>{doc.name}</h4>
+                        <p>Due by: {doc.dueDate.toDate().toDateString()}</p>
+                        <div className="assigned-to">
+                            <p><strong>Assigned to:</strong></p>
+                            <ul>
+                                {
+                                    doc.assignedUsersList.map((user, index) => (
+                                        <div key={index}>
+                                            <Avatar src={user.photoURL} />
+                                        </div>
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    </Link>
                 ))
             }
         </div>
